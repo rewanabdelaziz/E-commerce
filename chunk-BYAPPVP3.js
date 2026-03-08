14ad739a8db34b9ff17b9100bc37319e8eeb232c
Rewan Abd Elaziz <rewanabdelaziz9634@gmail.com>
2026-03-08 02:57:49 +0200

    Auto-generated commit

--- chunk-BYAPPVP3.js
+++
@@ -1,1 +0,0 @@
-var e={apiUrl:"https://fakestoreapi.com",production:!0,firebaseConfig:{apiKey:"AIzaSyC5iB1kmtf3WeaV0Xx4D5ywiWtMGPegt0w",authDomain:"e-commerce-1d3a7.firebaseapp.com",projectId:"e-commerce-1d3a7",storageBucket:"e-commerce-1d3a7.firebasestorage.app",messagingSenderId:"364711178541",appId:"1:364711178541:web:307408a0d8438c041b23a4",measurementId:"G-FHJ89L93TE"}};export{e as a};
