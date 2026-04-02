b1263469dc7886a7f7262092bc9e9eb3d6d9fb66
Rewan Abd Elaziz <rewanabdelaziz9634@gmail.com>
2026-04-02 22:11:44 +0200

    feat(auth): implement admin role override for specific accounts

--- src/app/auth/service/auth.service.ts
+++ src/app/auth/service/auth.service.ts
@@ -23,8 +23,13 @@ export class AuthService {
   async initAuth() {
     const token = localStorage.getItem('access_token');
     if (token) {
-      await this.getCurrentUser();
-      this.isLoggedIn.set(true)
+      const user = await this.getCurrentUser(); 
+      if (user) {
+        this.isLoggedIn.set(true);
+        this.userRole.set(user.role!); 
+      } else {
+        this.logout(); 
+      }
     }
   }
   
@@ -57,6 +62,9 @@ export class AuthService {
     try {
       // convert observable to promise
       const res = await firstValueFrom(this._http.get<UserProfile>(`${this.baseUrl}auth/profile`));
+      if (res.email == "john@mail.com"){
+          res.role = 'admin'
+      }
       this.currentUser.set(res);
       this.userRole.set(res.role!);
       this.isLoggedIn.set(true);
